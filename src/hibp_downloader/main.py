5228915da4ab022cff80be3475af2fdeb975085d
Nicholas de Jong <ndejong@threatpatrols.com>
2026-06-07 13:01:56 +1000

    Fix for Windows multiprocessing.freeze_support()

--- src/hibp_downloader/main.py
+++ src/hibp_downloader/main.py
@@ -2,6 +2,8 @@
 # Copyright [2022-2023] Threat Patrols Pty Ltd (https://www.threatpatrols.com)
 #
 
+import multiprocessing
+
 from . import LOGGER_NAME
 from .lib.app import invoke_app
 from .lib.logger import logger_get
@@ -10,4 +12,5 @@ logger = logger_get(LOGGER_NAME)
 
 
 def entrypoint():
+    multiprocessing.freeze_support()
     invoke_app()
